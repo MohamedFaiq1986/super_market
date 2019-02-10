e884a2254fb1ab7024fa3c0d7e6f52a7e66d4e1d
MohamedFaiq1986 <mohamedfaiq1986@gmail.com>
2019-02-10 19:26:54 +0300

    insert and delete

---
+++ src/superMar/DataOperations.java
@@ -0,0 +1,32 @@
+package superMar;
+
+import java.sql.ResultSet;
+public interface DataOperations {
+
+	// select data from table by table name.
+	ResultSet getData(String tableName);
+
+	// select data from table by table name and filter by id.
+	ResultSet getData(String tableName, String id);
+
+	// insert data in ItemSales table.
+	void insertItemSales(int id, int id2, int id3, int qty);
+
+	// insert data in ItemSales table.
+	void insertUsers(String id, String id2, int id3, int id4);
+
+	// insert data in table for 5 parameters.
+	void insertItems(int id, String item_name, int qty, Double price, Double selling_price);
+
+	// delete data from table by table name and filter by id.
+	void deleteData(String tableName, int id);
+
+	// delete data from table by table name and filter by id.
+	void UpdateData(String tableName, String colName, double data, int id);
+
+	// delete data from table by table name and filter by id.
+	void UpdateData(String tableName, String colName, int data, int id);
+
+	// delete data from table by table name and filter by id.
+	void UpdateData(String tableName, String colName, String data, int id);
+}
